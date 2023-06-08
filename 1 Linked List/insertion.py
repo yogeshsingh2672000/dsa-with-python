b4ddd392d1e1631c78ef37ccc047e89f42ce8694
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-06-08 21:11:10 +0530

    linkedList insertion

---
+++ 1 Linked List/insertion.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+    
+    def push(self, node):
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        else:
+            curr.next = node
+        return
+
+linkedList = LinkedList()
+linkedList.head = Node(1)
+linkedList.push(Node(2))
+linkedList.push(Node(3))
+linkedList.push(Node(4))
+
+curr = linkedList.head
+while curr.next:
+    print(curr.data)
+    nextPtr = curr.next
+    curr = nextPtr
